89423689fd4f4a1c3fa76eba56cbad25744bf6af
Rhotre <2235192@slu.edu.ph>
2026-02-18 14:56:53 +0800

    Adding the upload Card -harold

--- app/src/main/java/com/example/thesis/MainActivity.kt
+++ app/src/main/java/com/example/thesis/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.thesis.ui.theme.ThesisTheme
+import com.example.thesis.view.MainScaffolding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ThesisTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MainScaffolding()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ThesisTheme {
-        Greeting("Android")
+        MainScaffolding()
     }
 }
---
+++ app/src/main/java/com/example/thesis/view/topContent/parts/UploadCard.kt
@@ -0,0 +1,104 @@
+package com.example.thesis.view.topContent.parts
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.thesis.R
+
+@Composable
+fun UploadCard(){
+
+    Card(modifier = Modifier
+        .padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
+    )
+    {
+        Column (modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                shape = RoundedCornerShape(16.dp),
+                color = Color.Gray
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Spacer(modifier = Modifier.size(12.dp))
+            UploadButton()
+            UploadText()
+
+
+        }
+
+    }
+}
+
+@Composable
+fun UploadButton (){
+    Box(
+        contentAlignment = Alignment.Center
+    ){
+        Box(
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+                .background(Color(0xFF4F8DFF)),
+            contentAlignment = Alignment.Center
+        ){
+            Icon(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.size(32.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun UploadText(){
+    Column (modifier = Modifier.padding(16.dp)){
+        Text(
+            text = "Upload a Photo ",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            color = Color.Gray,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = "Upload a photo by importing or\nscanning with the camera",
+            fontSize = 14.sp,
+            color = Color.Gray,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
