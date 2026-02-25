13f1d55aa9f7394332e4604b05ffe7ef4042a73a
Jazper Bustria <2235111@slu.edu.ph>
2026-02-25 12:12:33 +0800

    dependecy fix and versions

--- app/src/main/java/com/example/thesis/view/MainScaffolding.kt
+++ app/src/main/java/com/example/thesis/view/MainScaffolding.kt
@@ -1,37 +1,41 @@
 package com.example.thesis.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.example.thesis.view.middleContent.MiddleContent
+import com.example.thesis.view.bottomNavigationBar.parts.BottomNavigationBar
 import com.example.thesis.view.topContent.TopContent
 
 @Composable
-fun MainScaffolding() {
+fun MainScaffolding(){
+    Scaffold (bottomBar = {
+        BottomAppBar {
+            BottomNavigationBar()
+            Text(
+                text = "show more"
+            )
+        }
+    }){
+            innerPadding ->
 
-    Scaffold(
-        bottomBar = {
-            BottomAppBar {
-                Text(text = "show more")
+
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+            )
+            {
+                TopContent()
             }
         }
-    ) { innerPadding ->
-
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-        ) {
-
-            TopContent()
-
-            MiddleContent()
-
-        }
-    }
 }
