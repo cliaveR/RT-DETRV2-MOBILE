7e4ab4fcd0a9123d6a46952fa79aa057f60a8f7f
Rhotre <2235192@slu.edu.ph>
2026-03-11 14:41:04 +0800

    Added a feature when taking a picture the location data is stored within it in CameraContent.kt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,11 +44,14 @@ dependencies {
     implementation(libs.volley)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.exifinterface)
+    implementation(libs.play.services.location)
     val camerax_version = "1.5.3"
     implementation("androidx.camera:camera-core:$camerax_version")
     implementation("androidx.camera:camera-camera2:$camerax_version") // ✅ this is what's missing
     implementation("androidx.camera:camera-lifecycle:$camerax_version")
     implementation("androidx.camera:camera-view:$camerax_version")
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -72,11 +75,9 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material.icons.extended)
-
     implementation("io.coil-kt:coil-compose:2.7.0")
- implementation("androidx.activity:activity-compose:1.12.4")
- implementation("androidx.compose.material3:material3:1.4.0")
-
+    implementation("androidx.activity:activity-compose:1.12.4")
+    implementation("androidx.compose.material3:material3:1.4.0")
     implementation(libs.arcgis.maps.kotlin)
     implementation(platform(libs.arcgis.maps.kotlin.toolkit.bom))
     implementation(libs.arcgis.maps.kotlin.toolkit.geoview.compose)
